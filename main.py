74783421b62750fb8ec9076e29d3e1f88904ae6f
waqar-haider-hol <waqar.haider@holcim.com>
2026-02-16 13:24:27 +0530

    first streamlit check

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import requests
+import json
 
-st.title("Hi I am streamlit web app")
+
+# configure streamlit page
+st.set_page_config(page_title="AI Chat with Amazon bedrock",
+                   page_icon="🤖",
+                   layout="centered")
+
